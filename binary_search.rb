8ec5847a31b8bb4328de1e19da180b9cc7c73a14
Jacob Yarborough <jayarborough95@gmail.com>
2022-03-27 21:29:15 -0600

    Add recursive binary search

--- binary_search.rb
+++ binary_search.rb
@@ -23,3 +23,25 @@ array1 = [1,4,8,13,42,56,78,93]
 val = 93
 
 puts binary_search(array1, val)
+
+
+# binary search recursively
+
+def binary_search_recursive(sorted_array, search_value, start_index, end_index)
+  return false if start_index > end_index
+
+  mid = (start_index + end_index)/2
+
+  return mid if sorted_array[mid] == search_value
+
+  if search_value < sorted_array[mid]
+    binary_search_recursive(sorted_array, search_value, start_index, mid - 1)
+  else 
+    binary_search_recursive(sorted_array, search_value, mid + 1, end_index)
+  end 
+end 
+
+array2 = [1,4,8,13,42,56,78,93]
+search_val = 93
+
+puts binary_search_recursive(array2, search_val, 0, array2.length - 1)
